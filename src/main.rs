cf304e16f4b9cb33dd05065ce5e779ef3ae46be2
Rimma Maksiutova <nnearobot@gmail.com>
2023-12-12 00:33:29 +0900

    Study: Building a Single-Threaded Web Server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+const SERVER_PORT: &str = "7878";
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", SERVER_PORT)).unwrap();
+    println!("Server has started on port {}", SERVER_PORT);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+
+    /*
+    let http_request: Vec<_> = buf_reader
+    .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html").unwrap();
+        let length = contents.len();
+        
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        
+        stream.write_all(response.as_bytes()).unwrap();
+    */
+
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(format!("static/{}", filename)).unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
