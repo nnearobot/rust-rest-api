d6c3593586bfa1ebd91b176f18b1742cfef36851
Rimma Maksiutova <nnearobot@gmail.com>
2023-12-21 23:32:36 +0900

    http module: response status line constants added

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,2 +1,5 @@
 pub mod method;
 pub mod router;
+
+pub const OK_RESPONSE: &str = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n\r\n";
+pub const NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
