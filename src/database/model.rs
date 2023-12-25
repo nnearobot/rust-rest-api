4689a3ab1f6cdaecf8fd0026bff838c8d93ed4d6
Rimma Maksiutova <nnearobot@gmail.com>
2023-12-26 01:41:58 +0900

    Model trait: execute() method with default implementation added

--- src/database/model.rs
+++ src/database/model.rs
@@ -1,4 +1,4 @@
-use postgres::Row;
+use postgres::{Row, types::ToSql};
 
 use crate::database;
 
@@ -11,6 +11,17 @@ pub trait Model {
             Ok(mut client) => 
                 match client.query(&format!("SELECT * FROM \"{}\"", Self::TABLE_NAME), &[]) {
                     Ok(items) => Ok(items),
+                    Err(error) => Err(error.to_string()),
+                },
+            _ => Err(database::CONECTION_ERROR.to_string()),
+        }
+    }
+
+    fn execute(query: &str, params: &[&(dyn ToSql + Sync)]) -> Result<u64, String> {
+        match database::client() {
+            Ok(mut client) => 
+                match client.execute(query, params) {
+                    Ok(rows) => Ok(rows),
                     Err(error) => Err(error.to_string()),
                 },
             _ => Err(database::CONECTION_ERROR.to_string()),
