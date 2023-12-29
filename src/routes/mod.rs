49bcac16bdd63b1374b83f86ff2fed98e66f6ee9
Rimma Maksiutova <nnearobot@gmail.com>
2023-12-29 15:59:22 +0900

    Health check: the endpoint for API health checking added

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -2,11 +2,17 @@ pub mod menu;
 pub mod tables;
 pub mod orders;
 
-use crate::http::router::Router;
+use crate::http::{router::Router, *};
 
 pub fn create(base: &'static str) -> Router<'static> {
     Router::new(base)
+        .get("/", health_check)
+
         .merge_from(menu::create())
         .merge_from(tables::create())
         .merge_from(orders::create())
+}
+
+fn health_check(_: &str, _: &Vec<&str>) -> (String, String) {
+    (OK_RESPONSE.to_string(), "Version 1 is running".to_string())
 }
